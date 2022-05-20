98cbfb73a9d945e5da4c7ce4d5dd8c94f7496112
Odai Wattad <45322451+odaiwa@users.noreply.github.com>
2022-05-20 19:12:40 +0300

    dir-shape for frontend

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth-area/login/login.component';
+import { RegisterComponent } from './components/auth-area/register/register.component';
+import { HomeComponent } from './components/layout-area/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,17 +2,36 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout-area/layout/layout.component';
+import { HomeComponent } from './components/layout-area/home/home.component';
+import { HeaderComponent } from './components/layout-area/header/header.component';
+import { FooterComponent } from './components/layout-area/footer/footer.component';
+import { PageNotFoundComponent } from './components/layout-area/page-not-found/page-not-found.component';
+import { AuthMenuComponent } from './components/auth-area/auth-menu/auth-menu.component';
+import { LoginComponent } from './components/auth-area/login/login.component';
+import { LogoutComponent } from './components/auth-area/logout/logout.component';
+import { RegisterComponent } from './components/auth-area/register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    AuthMenuComponent,
+    LoginComponent,
+    LogoutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LayoutComponent]
 })
 export class AppModule { }
